17602a7ed2f133d7500980d692df88063c5fca9b
fiorde90 <fiorde_90@hotmail.com>
2024-06-18 23:35:30 -0300

    DB conection and .env private enviroment variables

--- src/main/java/com/platzi/market/PlatziMarketApplication.java
+++ src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -1,5 +1,6 @@
 package com.platzi.market;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PlatziMarketApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("PSQL_PASS", dotenv.get("PSQL_PASS"));
 		SpringApplication.run(PlatziMarketApplication.class, args);
 	}
 

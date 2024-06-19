048be671fdf81786a1ceb230a38bfa95c8dec8ce
fiorde90 <fiorde_90@hotmail.com>
2024-06-19 17:48:08 -0300

    add tests

--- src/test/java/com/platzi/market/PlatziMarketApplicationTests.java
+++ src/test/java/com/platzi/market/PlatziMarketApplicationTests.java
@@ -1,13 +1,21 @@
 package com.platzi.market;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class PlatziMarketApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(context,"ApplicationContext shouldn't be null");
 	}
 
 }
---
+++ src/test/java/com/platzi/market/persistence/crud/ProductCrudRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.platzi.market.persistence.crud;
+
+import com.platzi.market.persistence.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class ProductCrudRepositoryTest {
+
+    @Autowired
+    ProductCrudRepository productCrudRepository;
+
+    @Test
+    void shouldReturnAProductListFromRepository() {
+        List<Product> productEntities = (List<Product>) productCrudRepository.findAll();
+        int productsNum = productEntities.size();
+        assertTrue(productsNum > 0);
+    }
+
+    @Test
+    void shouldReturnAListProductFromIdCategoryNativeQuery() {
+        List<Product> productEntities = productCrudRepository.getProductsByIdCategory(1);
+        int productsNum = productEntities.size();
+        assertEquals(10, productsNum);
+    }
+
+    @Test
+    void shouldReturnAListProductFromIdCategoryQueryJpaMethod() {
+        List<Product> productEntities = productCrudRepository.findByIdCategoryOrderByNameAsc(1);
+        int productsNum = productEntities.size();
+        assertEquals(10, productsNum);
+    }
+
+    @Test
+    void shouldReturnAListProductAreRunningOutOfStock() {
+        Optional<List<Product>> products = productCrudRepository.findByStockQuantityLessThanAndState(5, true);
+        long productsNum = products.stream().count();
+        assertEquals(1, productsNum);
+    }
+
+}

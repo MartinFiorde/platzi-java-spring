d6b7d30e5e34f189d103133f9661b14c63e4e8d3
fiorde90 <fiorde_90@hotmail.com>
2024-06-19 21:12:00 -0300

    name changes

--- src/main/java/com/platzi/market/domain/service/ProductService.java
+++ src/main/java/com/platzi/market/domain/service/ProductService.java
@@ -1,7 +1,7 @@
 package com.platzi.market.domain.service;
 
 import com.platzi.market.domain.dto.ProductDto;
-import com.platzi.market.domain.repository.ProductDtoRepository;
+import com.platzi.market.domain.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
@@ -12,32 +12,32 @@ import java.util.Optional;
 @Service
 public class ProductService {
 
-    private ProductDtoRepository productDtoRepository;
+    private ProductRepository productRepository;
 
     @Autowired
-    public ProductService(ProductDtoRepository productDtoRepository) {
-        this.productDtoRepository = productDtoRepository;
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
     public List<ProductDto> getAll() {
-        return productDtoRepository.getAll();
+        return productRepository.getAll();
     }
 
     public Optional<ProductDto> getProduct(long id) {
-        return productDtoRepository.getProduct(id);
+        return productRepository.getProduct(id);
     }
 
     public Optional<List<ProductDto>> getByCategory(long categoryId) {
-        return productDtoRepository.getByCategory(categoryId);
+        return productRepository.getByCategory(categoryId);
     }
 
     public ProductDto save(ProductDto product) {
-        return productDtoRepository.save(product);
+        return productRepository.save(product);
     }
 
     public boolean delete(long id) {
         try {
-            productDtoRepository.delete(id);
+            productRepository.delete(id);
             return true;
         } catch (EmptyResultDataAccessException e) {
             return false;

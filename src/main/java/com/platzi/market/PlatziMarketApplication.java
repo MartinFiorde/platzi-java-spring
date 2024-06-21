7914ef8c45eb5b0f968aa79b4514903269b6610b
fiorde90 <fiorde_90@hotmail.com>
2024-06-21 15:23:37 -0300

    fix to know ip

--- src/main/java/com/platzi/market/PlatziMarketApplication.java
+++ src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -3,11 +3,16 @@ package com.platzi.market;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 public class PlatziMarketApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
+		System.out.println("Application IP: " + InetAddress.getLocalHost().getHostAddress());
 		SpringApplication.run(PlatziMarketApplication.class, args);
+
 	}
 
 }

b76b7f63c32cf194de081fd63e7fcdc13ab2bda8
fiorde90 <fiorde_90@hotmail.com>
2024-06-21 12:32:10 -0300

    clean and changes to build and app.props. Load .env file from IDE

--- src/main/java/com/platzi/market/PlatziMarketApplication.java
+++ src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -1,6 +1,5 @@
 package com.platzi.market;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PlatziMarketApplication {
 
 	public static void main(String[] args) {
-
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("PSQL_PASS", dotenv.get("PSQL_PASS"));
 		SpringApplication.run(PlatziMarketApplication.class, args);
 	}
 
